d2247fe18b925f80e8d2aebcce90e518625e6a4c
damienbod <damien_bod@hotmail.com>
2019-01-23 16:46:07 +0100

    creating a view

---
+++ AspNetCoreBootstrap4Validation/Controllers/AjaxWithComponentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetCoreBootstrap4Validation.ViewModels;
+
+namespace AspNetCoreBootstrap4Validation.Controllers
+{
+    public class AjaxWithComponentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(StandardValidationModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return Redirect("Home/Index");
+        }
+
+        public IActionResult Playground()
+        {
+            return View();
+        }
+    }
+}
