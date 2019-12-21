1d25ad86aec6a380e444d855c40426893c208ff2
damienbod <damien_bod@hotmail.com>
2019-12-21 16:48:11 +0100

    code clean up

--- AspNetCoreBootstrap4Validation/Startup.cs
+++ AspNetCoreBootstrap4Validation/Startup.cs
@@ -1,32 +1,13 @@
-﻿using System;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using System.IdentityModel.Tokens.Jwt;
-using IdentityModel;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 
 namespace AspNetCoreBootstrap4Validation
 {
     public class Startup
     {
-        private readonly IWebHostEnvironment _environment;
-
-        public Startup(IWebHostEnvironment env)
-        {
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
-            _environment = env;
-        }
-
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-              .AddControllersWithViews()
-              .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app)
